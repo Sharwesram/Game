5d2fefd9710ed72ce544c9b156eb05b0cb935c00
Sharwesram <sharwesram@gmail.com>
2025-12-05 18:39:08 +0530

    Add files via upload

---
+++ program4.py
@@ -0,0 +1,16 @@
+# a="Sharwesram"
+# b=23
+# c=1.1
+# d=True
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# print(float(b))
+# print(int(c))
+
+a="Enter your name:"
+text=a[-14::-1]
+print(text)
