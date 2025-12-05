98337116e0023dccc4152fe28015206b108d91e4
Sharwesram <sharwesram@gmail.com>
2025-12-05 19:35:01 +0530

    Create project.py

---
+++ project.py
@@ -0,0 +1,19 @@
+# let us assume that 1st friend name is suraj, 2nd is ram, third is mohan,4th is kumar, 5th is sam
+a1="suraj"
+a2="ram"
+a3="mohan"
+a4="kumar"
+a5="sam"
+
+
+a=input("Enter the birthday date of friend 1:")
+b=input("Enter the birthday date of friend 2:")
+c=input("Enter the birthday date of friend 3:")
+d=input("Enter the birthday date of friend 4:")
+e=input("Enter the birthday date of friend 5:") 
+
+print(a1,a,",best of luck")
+print(a2,b,",best of luck")
+print(a3,c,",best of luck")
+print(a4,d,",best of luck")
+print(a5,e,",best of luck")
