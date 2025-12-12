0689200d2e2780a78616947b710c49e5c9ccc40e
Sharwesram <sharwesram@gmail.com>
2025-12-12 18:40:45 +0530

    Add files via upload

---
+++ operators3.py
@@ -0,0 +1,36 @@
+a=10
+b=10
+# if a == 10 and b == 0:
+#     print("True")
+
+
+# else:
+#     print("False")
+
+# if a<5 or b < 5 :
+#     print("True")
+
+# else:
+#     print("False") 
+
+#bmi body mark index weight upon height square
+
+# if a != 5 or b != 5:
+#     print("True")
+
+# else:
+#     print("False")
+
+w= 37
+h=1.53
+bmi=w/(h*h)
+print(bmi)
+if bmi < 18:
+    print("Underweight")
+
+elif bmi<25:
+    print("fit")
+
+else:
+    print("OVERWEIGHT")
+
