f44f4de2928137130bb16c7c58dd92bdc4847ff0
Sharwesram <sharwesram@gmail.com>
2025-12-14 12:10:30 +0530

    Add files via upload

---
+++ 12-12-25.py
@@ -0,0 +1,10 @@
+b=str(input("Enter a alphebet to check whether it is an alphabet or not:"))
+a=("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz")
+
+
+if(b in a):
+    print(f"yes {b} is an alphabet.")
+else:
+    print(f"no {b} is not an alphabet.")
+
+    
