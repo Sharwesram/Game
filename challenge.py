8b8768b5189bcf9aa98ee581e50b5b3b554514c0
Sharwesram <sharwesram@gmail.com>
2025-12-18 18:30:59 +0530

    Add files via upload

---
+++ challenge.py
@@ -0,0 +1,43 @@
+# 1st program :
+# a=1
+# b=2
+# c=3
+# d=4
+# print(((a+b)*c)/d)
+#2nd program:
+# name="sharwesram"
+# age=13
+# if name=="Sharwin" or name=="sharwesram" and age>18:
+#     print("welcome")
+# else:
+#     print("goodbye") 
+#program 3:
+# a1=10
+# a2=3
+# if a1 % a2 == 0:
+#     print("completely divisible.")
+
+# else:
+#     print("Not completely divisible.")
+#program 4:
+#mean of 40 numbers is 38 and one number is misread 56 as 36.
+# num=1520
+# s=1520+20
+# print((1520+20)/40,"\n",s)
+#program 5:
+#there are 3 cyclist they are riding at 10,20,30km/h find the mean and compare which one is slower than the average and low average.
+c1=10
+c2=10
+c3=30
+aver=(c1+c2+c3)/3
+print("average="+str(aver))
+if c1 < aver :
+    print("cyclist 1 is slower than the average speed.")
+
+a=0    
+if c2 < aver :
+    print("cyclist 2 is slower than the average speed.")
+
+a=1
+if c3 < aver :
+    print("cyclist 3 is slower than the average speed.")
