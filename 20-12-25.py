5868e39bef277622f3d4d2ac7b937c830ecb9244
Sharwesram <sharwesram@gmail.com>
2025-12-20 14:11:27 +0530

    Add files via upload

---
+++ 20-12-25.py
@@ -0,0 +1,8 @@
+age=int(input("Enter your age:"))
+if age >= 10:
+    if age <= 20 :
+        print("The age entered is between 10-20 years.")
+    else:
+        print("The age entered is not between 10-20.")
+else:
+    print("the age entered is not between 10-20 years.")
