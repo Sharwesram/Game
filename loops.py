f25c3128b77aa59e10ca38c01d1250ab4a7446ab
Sharwesram <sharwesram@gmail.com>
2025-12-22 18:29:12 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,22 @@
+# sum=0
+# t=int(input("Enter the number of times:"))
+# for i in range(1,t+1):
+#     sum += i
+#     print(sum)
+
+# print("sum=",sum)
+
+# a="12345678910"
+# b=""
+# for i in a:
+#     b=i+b
+    
+# print("Ordered string=",a)
+# print("reversed order=",b)
+
+# for i in range(10,0,-1):
+#   print(i)
+a=""
+for i in range (1,5):
+    a+="*"
+    print(a)
