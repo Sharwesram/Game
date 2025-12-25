66233f8f16710707defd93b6a52047afd908f1a3
Sharwesram <sharwesram@gmail.com>
2025-12-25 18:37:17 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,33 @@
+# num=int(input("Enter a number:"))
+# sum=0
+# i=1
+# while num >= i :
+#     sum=sum +i 
+#     i+=1
+#     print("sum=",sum)
+# a=10
+# while a>5:
+#     print(a)
+# num=int(input("Enter a Number:"))
+# sum=0
+# t=num
+# while t>0:
+#     digit=t%10
+#     sum += digit**3
+#     t //= 10
+# print("sum=",sum)
+
+# if sum==num:
+#     print("ArmStrong number!")
+# else:
+    # print("Not an ArmStrong Number!")
+# num=int(input("Enter a number:"))
+# t=1
+# while num>0 and t<=10:
+#     print(f"{num} x {t} = {num*t}")
+#     t += 1
+
+num=int(input("Enter a Number:"))
+while num>0:
+    print(num)
+    num -= 1
