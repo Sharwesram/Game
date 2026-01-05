fa15738bd500dfee04c48545fd42ccc2f9b73959
Sharwesram <sharwesram@gmail.com>
2026-01-05 19:36:47 +0530

    Add files via upload

---
+++ 5-1-26.py
@@ -0,0 +1,11 @@
+import turtle as t
+t.bgcolor("red")
+t.color("black","blue")
+t.fillcolor("blue")
+t.begin_fill
+for i in range(4):
+    t.right(90)
+    t.forward(90)
+t.fillcolor("blue")
+t.end_fill
+t.done()
