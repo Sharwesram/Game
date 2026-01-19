49608195455986b07c14139f9ea7f95e7162328a
Sharwesram <sharwesram@gmail.com>
2026-01-19 18:34:43 +0530

    Add files via upload

---
+++ key.py
@@ -0,0 +1,26 @@
+# for i in "SHARWESRAM":
+#     if i == "a".upper():
+#        break
+#     print(i)
+
+# num=int(input("Enter a num:"))
+# for i in range(21):
+#     if i % 5 == 0 :
+#         print("Fizz")
+#     elif i % 15 == 0 :
+#         pass
+#     elif i % 20 == 0 :
+#         print("Twist")
+#     elif i % 3 == 0 :
+#         print("Buzz")
+#     else:
+#         print(i)
+
+a=11
+while a>0 :
+    a-= 1
+    if a == 5 :
+        continue
+    print(a)
+   
+    
