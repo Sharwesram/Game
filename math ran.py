47c5647ef23b87413b9a94d6a3d10b9baa7d8d9b
Sharwesram <sharwesram@gmail.com>
2026-01-26 18:32:40 +0530

    Add files via upload

---
+++ math ran.py
@@ -0,0 +1,46 @@
+import random
+import math
+# g=random.randint(0,10)
+
+# while g>0:
+#       num=int(input("Guess the number:"))
+      
+      
+#       if num==g :
+#         print("You win!")
+#         break
+#       else:
+#           print("Your guess is wrong !")
+
+
+
+# a=["Rock","Paper","Scissor"]
+# while True:
+#     c=random.choice(a)
+#     yc=str(input("Rock or Paper or Scissor:")) 
+#     print(c) 
+#     if c==yc.lower:
+#         print("draw!")
+#     elif c=="Rock" and yc=="Paper".lower():
+#         print("You win !")
+#     elif c=="Rock" and yc=="scissor".lower() or yc=="scissors".lower:
+#         print("Computer wins!")
+#     elif c=="Paper".lower() and yc=="rock".lower():
+#         print("Computer wins !")
+#     elif c=="Paper" and yc=="Scissors".lower() or yc=="scissor".lower():
+#         print("You win!")
+#     elif c=="Scissor":
+#         if yc=="rock".lower():
+#             print("You win!")
+#         else:
+#             print("computer wins !")
+#     break
+a=0.5
+b=10
+c=-15
+print(math.floor(a))
+print(math.ceil(a))
+print(math.copysign(b,c))
+print(math.exp(b))
+print(math.factorial(b))
+print(math.gcd(5,10))
