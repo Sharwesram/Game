fd82a296acfa375f5c9903f8284bcb59ef3b7d50
Sharwesram <sharwesram@gmail.com>
2026-02-05 18:29:58 +0530

    Add files via upload

---
+++ dict..py
@@ -0,0 +1,30 @@
+# d={"ID1":
+#     {"name":"Sharwesram",
+#      "class":"VIII",
+#      "subject":"MATHS"},
+#      "ID2":
+#      {"name":"Sai",
+#       "class":"VIII",
+#       "subject":"Science"},
+#      "ID3":
+#      {"name":"Sharwesram",
+#          "class":"VIII",
+#          "subject":"MATHS"}}
+
+# empty={}
+# for key,value in d.items():
+#     if value not in empty.values():
+#         empty[key]=value
+# print(empty)
+
+# d2={'Codingal' : 2, 'is' : 2, 'best' : 2, 'for' : 2, 'Coding' : 1}
+# count=0
+# for key,value in d2.items():
+#     if d2[key]==2:
+#         count += 1
+# print(count)
+
+d3={'India' : '0091', 'Australia' : '0025', 'Nepal' : '00977'}
+print("India=",d3['India'])
+print("Australia=",d3['Australia'])
+print("Nepal=",d3['Nepal'])
