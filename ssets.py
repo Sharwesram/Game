c058e79d9417059297153a862f33234ccb8ef573
Sharwesram <sharwesram@gmail.com>
2026-02-06 18:30:55 +0530

    Add files via upload

---
+++ ssets.py
@@ -0,0 +1,24 @@
+# i={1,2,3,3,4,5}
+# print(i)
+# m={1,2.5,"ABC"}
+# print(m)
+# e={ 1, 2, 3, 4, 3, 2}
+# print(e)
+# l=set([1, 2, 3, 2])
+# print(l)
+# i.pop()
+# print(i)
+
+# a={1,2,3,4,5}
+# b={1,2,3,89,45}
+# print(a.intersection(b))
+import array as a
+
+b=a.array("i",[1, 3, 5, 3, 7, 9, 3])
+# print(b.count(3))
+# b.reverse()
+# print(b)
+
+
+for i in b:
+    print(i,end=",")
