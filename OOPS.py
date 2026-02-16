54aeb69eef3720e997ed96d53cfbcd7ae18bc8fa
Sharwesram <sharwesram@gmail.com>
2026-02-16 18:22:53 +0530

    Add files via upload

---
+++ OOPS.py
@@ -0,0 +1,30 @@
+# class IOSstring:
+#    def __init__(self,str1):
+#       self.str1=str1
+#    def str(self,s):
+#       self.s=s
+#    def convert(self,s):
+#       print(str(s).upper())
+# st=str(input("Enter a String:"))
+# ob1=IOSstring(st)
+# ob1.str(st)
+# ob1.convert(st)
+
+# class employee:
+#    def __init__(self):
+#       print("Employee ID created !")
+#    def __del__(self):
+#       pass
+
+# def calling():
+#    ob1=employee()
+# calling()
+
+class num:
+   t=(10,20,30,40,50,60,70)
+   sum=0
+   for i in t:
+      sum += i
+   print(list(enumerate(str(sum))))
+
+o=num()
